0797ca0f36d7ba085c9d0b8db0f47a9a6d89c31e
Paul <paul.baltarete99@e-uvt.ro>
2021-03-17 14:13:28 +0200

    Added search-bar, lesson and  tutor components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
+import { Lesson } from './lesson/lesson';
+import { Tutor } from './tutor/tutor';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'golearn';
+  tutors: Tutor[] = [
+    {
+      firstName: "Melinda",
+      lastName: "Popescu",
+      expertise: ["Matematica"],
+      hoursTutored: 20,
+      review: 5
+    },
+    {
+      firstName: "Ionut",
+      lastName: "Popescu",
+      expertise: ["Matematica", "Informatica"],
+      hoursTutored: 20,
+      review: 4
+    },
+    {
+      firstName: "Alin",
+      lastName: "Alinescu",
+      expertise: ["limba romana", "limba engleza"],
+      hoursTutored: 20,
+      review: 3
+    },
+    {
+      firstName: "Florin",
+      lastName: "Petrescu",
+      expertise: ["Geografie"],
+      hoursTutored: 20,
+      review: 4.5
+    },
+    {
+      firstName: "Melinda",
+      lastName: "Popescu",
+      expertise: ["Biologie"],
+      hoursTutored: 20,
+      review: 5
+    }
+  ]
 }
