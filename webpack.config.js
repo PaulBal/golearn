26dbc8a1d0734f17cda4143c16f0387a7bea7e8a
Paul <paul.baltarete99@e-uvt.ro>
2021-05-27 18:23:44 +0300

    removed tailwindcss from the project (I guess)

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,3 @@
-const { addTailwindPlugin } = require("@ngneat/tailwind");
-const tailwindConfig = require("./tailwind.config.js");
-
 module.exports = (config) => {
-  addTailwindPlugin({
-    webpackConfig: config,
-    tailwindConfig,
-    patchComponentsStyles: true
-  });
   return config;
 };
